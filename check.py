6394b4ff265a6c762cfc0968dfdf106a099df31b
Joe Truncale <josephtruncale@gmail.com>
2020-04-26 15:30:08 -0400

    csv writer

--- check.py
+++ check.py
@@ -1,3 +1,5 @@
+import csv
+
 import requests
 import time
 import datetime
@@ -5,30 +7,21 @@ import tweepy
 
 from bs4 import BeautifulSoup
 
-from utils import read_json
+from utils import read_json, tweet
+
 import authy
-
-creds = read_json('creds', message='twitter')
-
-auth = tweepy.OAuthHandler(creds['consumer_key'], creds['consumer_secret'])
-auth.set_access_token(creds['access_token'], creds['access_token_secret'])
-
-api = tweepy.API(auth)
 
 payload = "ControllerName=ReserveTimeslot&FulfillmentType=Pickup"
 
 headers = authy.authenticate()
 
 index = []
-
-# test store
-# store_list = { 'drexel': '1AD0123688'}
-
 counter = 0
 
 while 1:
     print(f'[attempt: {counter}] {datetime.datetime.now()}')
     store_list = read_json('stores')
+    # store_list = {'drexel': '1AD0123688'}
     for store_name in store_list:
         try:
             store_id = store_list[store_name]
@@ -47,7 +40,7 @@ while 1:
                 store_city = soup.find('span', class_='address__city')
                 store_region = soup.find('span', class_='address__region')
 
-                if store_name != None:
+                if store_name is not None:
                     store_name = store_name.get_text()
                     store_city = store_city.get_text()
                     store_region = store_region.get_text()
@@ -58,27 +51,29 @@ while 1:
 
                 num_slots = 0
                 for div in soup.find_all('div', class_='timeslotPicker__timeslotButton--wrap timeslotPicker__cell'):
-                    text = str.strip(div.get_text())
-                    if text != 'Sold Out':
+                    details = str.strip(div.get_text())
+                    if details != 'Sold Out':
                         header = f'[{store_name}, {store_city}, {store_region}]'
-                        body = f'[{header}]\n{text}'
-                        slot_id = header + text
+                        slot_id = header + details
                         if slot_id not in index:
                             num_slots += 1
                             index.append(slot_id)
-                            print(f'TimeSlot Available: {text}')
+                            print(f'TimeSlot Available: {details}')
 
+                # tweet or clean up
                 if num_slots == 1:
-                    api.update_status(body)
+                    tweet(header, num_slots, store_city, details)
                 elif num_slots > 1:
-                    api.update_status(f'[{header}]\n{num_slots} available slots')
+                    tweet(header, num_slots, store_city)
                 else:
                     # clear out index for that store when no slots are found
                     for slot_id in index:
                         if slot_id.startswith(store_name):
                             index.remove(slot_id)
+            #
             else:
                 # if non-200 re-authenticate
+                print(f'Error: {res.status_code} response. Trying to re-authenticate')
                 headers = authy.authenticate()
         except Exception as e:
             print(f'Error on request: {e}')
--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
+import csv
+import datetime
 import json
+import os
+
+import tweepy
 
 
 def read_json(name, message=''):
@@ -7,3 +12,37 @@ def read_json(name, message=''):
     file.close()
     print(f'reading {name} {message}')
     return data
+
+
+def tweet(header, count, store_city, details=''):
+    creds = read_json('creds', message='twitter')
+
+    auth = tweepy.OAuthHandler(creds['consumer_key'], creds['consumer_secret'])
+    auth.set_access_token(creds['access_token'], creds['access_token_secret'])
+
+    api = tweepy.API(auth)
+
+    body = f'{header}\n{count} available slots\n{details}'
+
+    row = [store_city, count, datetime.datetime.now()]
+    write_csv(row)
+    api.update_status(body)
+
+
+def write_csv(row):
+    # name of csv file
+    filename = "config/results.csv"
+    headers = ['store', 'count', 'timestamp']
+
+    file_exists = os.path.isfile(filename)
+
+    # writing to csv file
+    with open(filename, 'a') as csvfile:
+        # creating a csv writer object
+        csvwriter = csv.writer(csvfile)
+
+        # writing the headers
+        if not file_exists:
+            csvwriter.writerow(headers)
+
+        csvwriter.writerow(row)
